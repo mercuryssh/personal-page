9285bbb7f6ce63aea24ca9eb881c3781fb211c45
Joseph Estevez <mateobackground@gmail.com>
2021-08-25 07:58:44 -0400

    add tags in projects preview

--- src/components/projects/itemCart.tsx
+++ src/components/projects/itemCart.tsx
@@ -4,9 +4,13 @@ type ItemCardProps = {
   title: string;
   cover: string;
   description: string;
+  url: string;
+  tags: Array<any>;
 };
 
 export default function ItemCard(props: ItemCardProps) {
+  console.log(props.tags);
+  const tags = Object.keys(props.tags)
   return (
     <div className="item-card">
       <figure>
@@ -14,10 +18,12 @@ export default function ItemCard(props: ItemCardProps) {
       </figure>
       <div className="content">
         <p className="title">{props.title}</p>
-        <p>{props.description}</p>
         <hr />
+        <p>{props.description}</p>
         <div className="tag-list">
-          <Tag title={"project tag"} />
+          {tags.map((item, index) => (
+            <Tag title={item} key={index} />
+          ))}
         </div>
       </div>
     </div>
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,15 +1,28 @@
 import ItemCard from "../../components/projects/itemCart";
 import Layout from "../../components/structure/layout";
 
+export const getStaticProps = async () => {
+  const projects = await (await fetch("https://mashu-blog.herokuapp.com/projects")).json();
+
+  return {
+    props: { projects },
+  };
+};
+
 const Index = (props) => {
   return (
     <Layout title="Proyects">
       <div className="project-container">
-        <ItemCard
-          title="proyect name"
-          description="project template"
-          cover={"ryoko matoi.png"}
-        />
+        {props.projects.map((item, index) => (
+          <ItemCard
+            title={item.name}
+            description={item.description}
+            cover={item.cover_url}
+            url={item.url_name}
+            key={index}
+            tags={item.tags}
+          />
+        ))}
       </div>
     </Layout>
   );
